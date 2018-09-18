601b3fef8a64bae1cc61acd8b1a3ffb483619d22
Luc Brulet <luc.brulet@epitech.eu>
2018-09-18 14:15:06 +0200

    show all user when login on the console.log

--- src/userSchema.js
+++ src/userSchema.js
@@ -1,7 +1,30 @@
 var mongoose = require('mongoose');
+var jwt = require('jsonwebtoken');
 
-exports.UserSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
-  }, { timestamps: true });
+var UserSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+}, { timestamps: true });
+
+UserSchema.methods.generateJWT = function () {
+  const today = new Date();
+  const expirationDate = new Date(today);
+  expirationDate.setDate(today.getDate() + 60);
+
+  return jwt.sign({
+    email: this.email,
+    id: this._id,
+    exp: parseInt(expirationDate.getTime() / 1000, 10),
+  }, 'secret');
+}
+
+UserSchema.methods.toAuthJSON = function() {
+  return {
+    _id: this._id,
+    email: this.email,
+    token: this.generateJWT(),
+  };
+};
+
+exports.UserSchema = UserSchema;
