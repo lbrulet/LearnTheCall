4a938b718e49c8927afdc7577f2fc61d5d211f30
Luc Brulet <luc.brulet@epitech.eu>
2018-09-29 17:23:48 +0200

    oui

--- charriot/src/main.js
+++ charriot/src/main.js
@@ -1,7 +1,12 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
