4ea366a6d148591fb3f6d68683ab333e5c8d8a24
Corentin <corentin.charriot@epitech.eu>
2018-11-29 17:11:50 +0100

    game vue

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,36 +2,31 @@
   <main>
     <v-app dark>
       <h1>RAINBOW SIX SIEGE</h1>
-      <v-container grid-list-md text-xs-center>
-        <v-layout>
-          <v-flex xs8>
-            <v-card-media v-bind:src="require(`@/assets/ImageR6/${activeImage}`)"></v-card-media>
-          </v-flex>
-          <v-flex>
-            <v-card @click.native="pickOneImage()" class="mb-2" height="24%" dark color="secondary">
-              <v-card-text class="pa-5 mb-2">{{ responseImg }}</v-card-text>
-            </v-card>
-            <v-card @click.native="pickOneImage()" class="mb-2" height="24%" dark color="secondary">
-              <v-card-text class="mb-2">
-                <p class="text-xs-center">3</p>
-              </v-card-text>
-            </v-card>
-            <v-card @click.native="pickOneImage()" class="mb-2" height="24%" dark color="secondary">
-              <v-card-text class="pa-5 mb-2">3</v-card-text>
-            </v-card>
-            <v-card @click.native="pickOneImage()" class="mb-2" height="24%" dark color="secondary">
-              <v-card-text class="pa-5 mb-2">3</v-card-text>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
+      <div id='GameContainer'>
+        <div class="GameItem" style="height: auto; min-width: 0;">
+            <img v-bind:src="require(`@/assets/ImageR6/${activeImage}`)" style="width: 900px; max-width: 100%; max-height: 500px; margin: 10px;">
+        </div>
+        <div class="GameItem" style="margin-top: 12px;">
+            <div class="hoverOnResponse" @click="clickOnAnswer(allAnswer[0], 0)" :class="{badResponse: correct[0] == 1, goodResponse: correct[0] == 2}" style="width: 300px; height: 120px; line-height: 120px; border: 1px solid white; margin-bottom: 5px; margin-right: 30px;">
+              <p style="text-align: center;">{{allAnswer[0]}}</p>
+            </div>
+            <div class="hoverOnResponse" @click="clickOnAnswer(allAnswer[1], 1)" :class="{badResponse: correct[1] == 1, goodResponse: correct[1] == 2}" style="width: 300px; height: 120px; line-height: 120px; border: 1px solid white; margin-bottom: 5px; margin-right: 30px;">
+              <p style="text-align: center;">{{allAnswer[1]}}</p>
+            </div>
+            <div class="hoverOnResponse" @click="clickOnAnswer(allAnswer[2], 2)" :class="{badResponse: correct[2] == 1, goodResponse: correct[2] == 2}" style="width: 300px; height: 120px; line-height: 120px; border: 1px solid white; margin-bottom: 5px; margin-right: 30px;">
+              <p style="text-align: center;">{{allAnswer[2]}}</p>
+            </div>
+            <div class="hoverOnResponse" @click="clickOnAnswer(allAnswer[3], 3)" :class="{badResponse: correct[3] == 1, goodResponse: correct[3] == 2}" style="width: 300px; height: 120px; line-height: 120px; border: 1px solid white; margin-bottom: 5px; margin-right: 30px;">
+              <p style="text-align: center;">{{allAnswer[3]}}</p>
+            </div>
+        </div>
+      </div>
     </v-app>
   </main>
 </template>
 
-
 <script>
-var wrongAnswer = [
+var allWrongAnswer = [
   "BedRoom",
   "Wine Cellar",
   "Garage",
@@ -52,8 +47,11 @@ export default {
   data() {
     return {
       allImages: [],
+      allAnswer: [],
       responseImg: "",
-      activeImage: "loading.jpg"
+      activeImage: "chalet00.jpg",
+      correct: [],
+      test: 1
     };
   },
   mounted() {
@@ -72,22 +70,50 @@ export default {
         }
       })
       .catch(err => {});
+    this.pickOneImage();
   },
   methods: {
     pickOneImage() {
+      for (var i = 0; i < 4; i++) this.correct.pop();
       if (this.allImages.length > 0) {
         let i = this.randomNumber(this.allImages.length);
         let stock = this.allImages[i];
         this.allImages.splice(this.allImages.indexOf(stock), 1);
         this.activeImage = stock;
-        console.log("stock = " + stock)
+        console.log("stock = " + stock);
         this.getThisImage(stock);
       } else {
         console.log("Il n'y a plus d'images a afficher !");
       }
     },
+    clickOnAnswer(response, index) {
+      console.log("la reponse cliqué est =", response);
+      var pos;
+      for (pos = 0; pos < this.allAnswer.length; pos++) {
+        if (this.responseImg == this.allAnswer[pos]) break;
+      }
+      for (var i = 0; i < 4; i++) {
+        if (pos == i) this.correct.push("2");
+        else this.correct.push("1");
+      }
+      console.log("correct == ", this.correct);
+      var self = this;
+      setTimeout(function() {
+        self.pickOneImage();
+      }, 1000);
+    },
     randomNumber(max) {
       return Math.floor(Math.random() * (max - 1 + 1));
+    },
+    mixArray(a) {
+      var j, x, i;
+      for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+      }
+      return a;
     },
     getThisImage(ImageName) {
       axios
@@ -98,18 +124,28 @@ export default {
         .then(response => {
           if (response.data.message == "Error") {
             console.log("Error to get this Image !");
-            this.responseImg = "Nothing"
+            this.responseImg = "Nothing";
           } else {
-            this.responseImg = response.data.message.goodAnswer
-            console.log(this.responseImg);
+            this.responseImg = response.data.message.goodAnswer;
+            this.allAnswer = this.getWrongAnswer();
+            this.allAnswer.push(this.responseImg);
+            this.allAnswer = this.mixArray(this.allAnswer);
           }
         })
         .catch(err => {
-          console.log(err)
+          console.log(err);
         });
     },
     getWrongAnswer() {
-      var tab = wrongAnswer.slice();
+      var tab = allWrongAnswer.slice();
+      console.log(tab);
+      var res = [];
+      for (var i = 0; i < 3; i++) {
+        var rdm = this.randomNumber(tab.length);
+        res[i] = tab[rdm];
+        tab.splice(tab.indexOf(res[i]), 1);
+      }
+      return res;
     }
   }
 };
@@ -119,4 +155,41 @@ export default {
 .card {
   height: 100px;
 }
+
+/* Pour cacher la scrollbar de droite ... */
+::-webkit-scrollbar {
+  width: 0px;
+  background: transparent;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #ff0000;
+}
+/* fin */
+
+.goodResponse {
+  background-color: green !important;
+}
+
+.badResponse {
+  background-color: red !important;
+}
+
+.hoverOnResponse:hover {
+  background-color: black;
+  opacity: 0.4;
+}
+
+/* FlexBox */
+#GameContainer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.GameItem {
+  flex: 0 1 auto;
+}
+/* FlexBox */
 </style>
